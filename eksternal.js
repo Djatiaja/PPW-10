8f0035709bcc898257dd9891f0249f29dc69db10
djatiaja <djamgt23@gmail.com>
2024-05-22 21:58:57 +0700

    don

---
+++ eksternal.js
@@ -0,0 +1,9 @@
+function gantiIsi(id, isi){
+    let temp = document.getElementById(id)
+    temp.innerHTML = isi
+
+}
+function tambah(){
+    temp2 = document.getElementById("box")
+    temp2.innerHTML += "<p>lorem ipsum dolor amet </p>"
+}
